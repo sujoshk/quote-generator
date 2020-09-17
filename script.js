4bcd777f121768bb3f2b4814a0a0b91adc5b72c5
Sujosh <sujoshk@gmail.com>
2020-09-17 13:32:55 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('This is the best time of my life.')
+// Get quote from API 
+
+async function getQuote() {
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+
+    try {
+        
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        getQuote();
+        console.log('Whoops, no quote', error);
+    }
+
+}
+
+
+//On Load
+
+getQuote();
